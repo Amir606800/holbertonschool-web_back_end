21a81a9a74f3d488a82d65740b15372702209b30
root <asadliamir800@gmail.com>
2025-11-29 20:06:33 +0300

    Changes saved

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """ uSING mONGOdB WITH PYTHON """
+
+
 def list_all(mongo_collection):
+    """ checking the collection status """
+
     if mongo_collection.find():
         return mongo_collection.find()
     return []
