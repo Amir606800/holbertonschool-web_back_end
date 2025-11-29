0a0bf795f2c1123038cd5809ca1925a27558e068
root <asadliamir800@gmail.com>
2025-11-29 20:29:58 +0300

    checking

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -5,5 +5,5 @@
 def insert_school(mongo_collection, **kwargs):
     """ Inserting and retrieving the id """
 
-    new_id = mongo_collection.insertOne(**kwargs)
+    new_id = mongo_collection.insert_one(**kwargs)
     return new_id
