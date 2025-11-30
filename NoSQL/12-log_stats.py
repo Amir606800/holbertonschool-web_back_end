66d2a8d6d2c941d1a14417c027ab44c3036fc141
root <asadliamir800@gmail.com>
2025-11-30 12:33:03 +0400

    Seemed complex but it was alright

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Pretty logs in python with Mongo """
+from pymongo import MongoClient
+
+
+def count(mg_col, method):
+    """ Writing an simple count function for reducing the repetition """
+
+    res = mg_col.count_documents({"method": method})
+    return res
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    ng_collection = client.logs.nginx
+
+    print(f"{ng_collection.count_documents({})} logs")
+    print("Method:")
+    for i in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print(f"    method {i}: {count(ng_collection, i)}")
+    print("{} status check".format(ng_collection.count_documents({"path": "/status"})))
+
